674336ed5e8917f30f8be0023251c792993acd7e
Nalinihatti <nalinihatti@gmail.com>
2026-03-11 12:36:13 +0530

    operators

---
+++ operators/Arith1.java
@@ -0,0 +1,11 @@
+public class Arith1{
+    public static void main(String[] args){
+        //Operators in java
+        // Arithmatic Operators --> +, -, *, /, %, +=, -=, /=, %=, ++, --
+
+        int a = 5;
+        int b = 10;
+
+        int c = a + b; //15
+    }
+}
