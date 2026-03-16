deb0c549c8250a576d188b23c9efef091ce80d0d
Nalinihatti <nalinihatti@gmail.com>
2026-03-16 22:43:39 +0530

    Create If1.java

---
+++ conditionals/If1.java
@@ -0,0 +1,14 @@
+public class If1{
+    public static void main(String[] args){
+        int i = 6;
+
+        // Selection statements
+        // Normal if else
+        if(i > 5){
+            System.out.println("i is greater than 5");
+        }
+        else{
+            System.out.println("i is not greater than 5");
+        }
+    }
+}
