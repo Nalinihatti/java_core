97e285fbd98ce47523d1903be86861d2085ca1dc
Nalinihatti <nalinihatti@gmail.com>
2026-03-16 22:43:46 +0530

    Create If2.java

---
+++ conditionals/If2.java
@@ -0,0 +1,12 @@
+public class If2{
+    public static void main(String[] args) {
+        int i = 6;
+
+        if(i % 2 == 0){
+            System.out.println("i is even");
+        }
+        else{
+            System.out.println("i is odd");
+        }
+    }
+}
